1e4b2f1fb17bccb0683f5a42b92b9c8ca4731d12
serhatkutlu <84921644+serhatkutlu@users.noreply.github.com>
2022-04-19 23:46:39 +0300

    slidable pagination added

---
+++ app/src/main/java/com/msk/itunes/ui/SearchScreen/component/TabScreen.kt
@@ -0,0 +1,79 @@
+package com.msk.itunes.ui.SearchScreen.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.TabRowDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.pager.*
+import kotlinx.coroutines.launch
+
+@ExperimentalPagerApi
+@Composable
+fun TabScreen() {
+    val pagerState = rememberPagerState(pageCount = 3)
+    Column(
+        modifier = Modifier.background(Color.White)
+    ) {
+        Tabs(pagerState = pagerState)
+        TabsContent(pagerState = pagerState)
+    }
+}
+
+@ExperimentalPagerApi
+@Composable
+fun Tabs(pagerState: PagerState) {
+    val list = listOf("")
+    val scope = rememberCoroutineScope()
+
+    TabRow(
+        selectedTabIndex = pagerState.currentPage,
+        contentColor = Color.White,
+        divider = {
+            TabRowDefaults.Divider(
+                thickness = 2.dp,
+                color = Color.Green
+            )
+        },
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                Modifier.pagerTabIndicatorOffset(pagerState, tabPositions),
+                height = 2.dp,
+                color = Color.White
+            )
+        }
+    ) {
+        list.forEachIndexed { index, _->
+            Tab(
+                text = {
+                    Text(
+                        list[index],
+                        color = if (pagerState.currentPage == index) Color.White else Color.LightGray
+                    )
+                },
+                selected = pagerState.currentPage == index,
+                onClick = {
+                    scope.launch {
+                        pagerState.animateScrollToPage(index)
+                    }
+                }
+            )
+        }
+    }
+}
+
+@ExperimentalPagerApi
+@Composable
+fun TabsContent(pagerState: PagerState) {
+    HorizontalPager(state = pagerState) { page ->
+        when(page) {
+
+        }
+    }
+}
