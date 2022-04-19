57baf19bdb54d4743a82457639184440400c0fc8
serhatkutlu <84921644+serhatkutlu@users.noreply.github.com>
2022-04-19 23:43:48 +0300

    viewmodel funcs added

--- app/src/main/java/com/msk/itunes/ui/SearchScreen/SearchScreenViewModel.kt
+++ app/src/main/java/com/msk/itunes/ui/SearchScreen/SearchScreenViewModel.kt
@@ -3,29 +3,73 @@ package com.msk.itunes.ui.SearchScreen
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.msk.itunes.Api.itunesApi
 import com.msk.itunes.Repository.SearchRepository
-import dagger.hilt.android.HiltAndroidApp
+import com.msk.itunes.Responce.Data.SearcResponce.Result
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
+
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlin.coroutines.CoroutineContext
 
 @HiltViewModel
 class SearchScreenViewModel @Inject constructor ( private val repository: SearchRepository):ViewModel() {
 
-    fun get() {
-        viewModelScope.launch() {
-            val a=repository.Search("aleyna").collect{
-                it.onSuccess {
-                    it.forEach { t, u ->
-                        Log.d("hatalar",t)
-                    }
 
-                }
+    private val _MapData: MutableStateFlow<MutableMap<String, MutableList<Result>>?> =
+        MutableStateFlow(null)
+    val MapData: StateFlow<MutableMap<String, MutableList<Result>>?> = _MapData
+
+     //var searchquery: MutableStateFlow<String> = MutableStateFlow("")
+    private var searchquery=""
+
+    private var currentPage = 0
+
+    private var pageJob: Job? = null
+    private var searchJob: Job? = null
+
+
+    fun OnEvent(SearchEvent: SearchEvent) {
+        when (SearchEvent) {
+            is SearchEvent.LoadPage -> {
+                LoadNewPage()
             }
+            is SearchEvent.Searchquery->{
+                onsearch(SearchEvent.query)
+            }
+        }
+    }
+
+    private fun LoadNewPage() {
+        if (pageJob?.isActive ?: false) {
+            return
+        }
+        pageJob = viewModelScope.launch {
+            repository.Search(searchquery, currentPage).onEach {
+
+                it.onSuccess {
+                    _MapData.value = it
+                    currentPage+=25
+                }
+
+            }.launchIn(this)
+
+
+        }
+    }
+
+
+    private fun onsearch(query: String) {
+        searchquery= query
+        searchJob?.cancel()
+        searchJob = viewModelScope.launch {
+            pageJob = null
+            currentPage=0
+            LoadNewPage()
 
         }
     }
