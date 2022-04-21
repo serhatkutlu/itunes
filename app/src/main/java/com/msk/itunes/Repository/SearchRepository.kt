dcbceb9596b70beed1ee1c4e660377d789cb61b8
serhatkutlu <84921644+serhatkutlu@users.noreply.github.com>
2022-04-21 22:52:27 +0300

    Mediatype changed

--- app/src/main/java/com/msk/itunes/Repository/SearchRepository.kt
+++ app/src/main/java/com/msk/itunes/Repository/SearchRepository.kt
@@ -1,12 +1,10 @@
 package com.msk.itunes.Repository
 
 import com.msk.itunes.Responce.Data.SearcResponce.track.SearchResponce
-import com.msk.itunes.Responce.Data.WrapperTypeData
-import com.msk.itunes.Util.MediaType
 import kotlinx.coroutines.flow.Flow
 
 interface SearchRepository {
 
-    fun  Search(query:String,offset:Int,Mediatype: MediaType): Flow<Result<SearchResponce>>
+    fun  Search(query:String,offset:Int,type: String): Flow<Result<SearchResponce>>
 
     }
--- app/src/main/java/com/msk/itunes/Repository/SearchRepositoryImp.kt
+++ app/src/main/java/com/msk/itunes/Repository/SearchRepositoryImp.kt
@@ -2,19 +2,17 @@ package com.msk.itunes.Repository
 
 import com.msk.itunes.Api.itunesApi
 import com.msk.itunes.Responce.Data.SearcResponce.track.SearchResponce
-import com.msk.itunes.Responce.Data.WrapperTypeData
-import com.msk.itunes.Util.MediaType
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class SearchRepositoryImp @Inject constructor(private val api:itunesApi):SearchRepository {
 
-   override fun  Search(query:String,offset:Int,Mediatype:MediaType): Flow<Result<SearchResponce>> {
+   override fun  Search(query:String,offset:Int,type:String): Flow<Result<SearchResponce>> {
 
        return flow {
            try {
-               val responce=api.searchItunes(query, offset = offset, media =Mediatype.type )
+               val responce=api.searchItunes(query, offset = offset, media =type )
                emit(Result.success(responce))
            }catch (e:Exception){
                emit(Result.failure(e))
