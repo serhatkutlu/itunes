d05bc1b1c9c664fae71585597d9a66838f493c70
serhatkutlu <84921644+serhatkutlu@users.noreply.github.com>
2022-04-21 22:53:40 +0300

    created new pagination and viewmodel structure

--- app/src/main/java/com/msk/itunes/ui/SearchScreen/SearchScreen.kt
+++ app/src/main/java/com/msk/itunes/ui/SearchScreen/SearchScreen.kt
@@ -1,24 +1,37 @@
 package com.msk.itunes.ui.SearchScreen
 
+import android.util.Log
 import androidx.compose.animation.*
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Image
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
 import com.google.accompanist.pager.*
+import com.msk.itunes.Data.MediaTypeDataClass
+import com.msk.itunes.Responce.Data.SearcResponce.track.Result
+import com.msk.itunes.Util.changeImageQuality
 import com.msk.itunes.ui.SearchScreen.component.Searchbar
-import com.msk.itunes.ui.SearchScreen.component.TabScreen
+import kotlinx.coroutines.flow.collect
 
 
 @OptIn(ExperimentalPagerApi::class)
@@ -49,13 +62,89 @@ fun SearchScreen(navController: NavHostController) {
 
         }
 
-        TabScreen()
+
+        SearchListContentScreen(viewModel)
 
 
     }
 
 
+}
 
+@Composable
+fun SearchListContentScreen(viewModel: SearchScreenViewModel) {
+
+    val mediaData=viewModel.MediaData.collectAsState()
+    val columnstate= rememberLazyListState()
+    LazyColumn(modifier = Modifier.fillMaxSize(), state = columnstate) {
+        item{
+            Spacer(Modifier.height(30.dp))
+        }
+        items(mediaData.value.size){index ->
+            if (mediaData.value[index].result.isNullOrEmpty()) return@items
+            MediaTypeRowScreen(mediaData.value[index])
+            Spacer(Modifier.height(10.dp))
+        }
+        item{
+            Spacer(Modifier.height(70.dp))
+        }
+
+    }
+
+}
+@Composable
+fun MediaTypeRowScreen(mediaTypeDataClass: MediaTypeDataClass) {
+Column(modifier = Modifier.fillMaxWidth().height(300.dp)) {
+    Box(modifier = Modifier.fillMaxWidth().padding(horizontal = 30.dp)){
+        Text(mediaTypeDataClass.mediaType, style = MaterialTheme.typography.h4, modifier = Modifier.align(
+            Alignment.CenterStart))
+        Icon(Icons.Default.ArrowForward,contentDescription = null, modifier = Modifier.align(
+            Alignment.CenterEnd).size(40.dp))
+
+    }
+    HorizontalList(mediaTypeDataClass.result)
+}
+
+}
+@Composable
+fun HorizontalList(result: List<Result>) {
+
+    val state=LazyListState()
+    LazyRow (state = state){
+        items(result.size){item->
+            BoxScreen(result[item],Modifier.height(300.dp).width(150.dp).padding(horizontal = 5.dp))
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class, coil.annotation.ExperimentalCoilApi::class)
+@Composable
+fun BoxScreen(result: Result,modifier:Modifier) {
+    Column (modifier = modifier){
+        Card(shape = AbsoluteRoundedCornerShape(10.dp), modifier = Modifier.fillMaxWidth().fillMaxHeight(0.8f), onClick = {}) {
+            val painter= rememberImagePainter(data=result.artworkUrl100?.changeImageQuality("200x200"))
+            when(painter.state){
+                is ImagePainter.State.Loading->{
+                    Box(modifier= Modifier.fillMaxSize(0.3f)) {
+                        CircularProgressIndicator(modifier= Modifier.align(Alignment.Center))
+                    }
+                }
+                is ImagePainter.State.Error->{
+                    Image(Icons.Default.Image,contentDescription = "error")
+                }
+                else->{
+                    Image(painter,contentDescription = null,modifier= Modifier.fillMaxSize(), contentScale = ContentScale.FillBounds)
+
+                }
+
+
+            }
+        }
+        Box(modifier = Modifier.fillMaxWidth().height(30.dp).align(Alignment.CenterHorizontally).padding(horizontal = 5.dp)){
+            Text(text = result.trackName ?: result.collectionName ?:"", style = MaterialTheme.typography.h6, overflow = TextOverflow.Ellipsis, textAlign = TextAlign.Center, maxLines = 2, modifier = Modifier.align(Alignment.Center))
+        }
+
+    }
 }
 
 
--- app/src/main/java/com/msk/itunes/ui/SearchScreen/SearchScreenViewModel.kt
+++ app/src/main/java/com/msk/itunes/ui/SearchScreen/SearchScreenViewModel.kt
@@ -3,16 +3,16 @@ package com.msk.itunes.ui.SearchScreen
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.msk.itunes.Data.MediaTypeDataClass
 import com.msk.itunes.Repository.SearchRepository
-import com.msk.itunes.Responce.Data.WrapperTypeData
+import com.msk.itunes.Util.Constants
 import com.msk.itunes.ui.SearchScreen.component.SearchState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,78 +20,79 @@ import javax.inject.Inject
 class SearchScreenViewModel @Inject constructor ( private val repository: SearchRepository):ViewModel() {
 
 
-    private val _WrapperData: MutableStateFlow<WrapperTypeData> =
-        MutableStateFlow(WrapperTypeData())
-    val WrapperData: StateFlow<WrapperTypeData> = _WrapperData
+    private val _MediaData: MutableStateFlow<MutableList<MediaTypeDataClass>> = MutableStateFlow(mutableListOf())
+    val MediaData: MutableStateFlow<MutableList<MediaTypeDataClass>> =_MediaData
 
     private val _SearchStates:MutableStateFlow<SearchState> = MutableStateFlow(SearchState())
     val SearchState:StateFlow<SearchState> =_SearchStates
 
 
-    private var searchquery=""
-
-    private var currentPage = 0
-
-    private var pageJob: Job? = null
-    private var searchJob: Job? = null
-
 
     fun OnEvent(SearchEvent: SearchEvent) {
         when (SearchEvent) {
-            is SearchEvent.LoadPage -> {
-                LoadNewPage()
-            }
             is SearchEvent.Searchquery->{
                 onsearch(SearchEvent.query)
             }
         }
     }
 
-    private fun LoadNewPage() {
-        if (pageJob?.isActive ?: false||searchquery.isBlank()) {
-            return
-        }
-        _SearchStates.value=SearchState.value.copy(isLoading = true)
-        pageJob = viewModelScope.launch {
-            repository.Search(searchquery, currentPage).onEach {
 
-                it.onSuccess {
-                    OnSucces(it)
-                }
-                it.onFailure {
-                    Log.d("hata",it.localizedMessage)
-                    _SearchStates.value=SearchState.value.copy(isLoading = false)
-                }
-
-            }.launchIn(this)
-
-
-        }
-    }
-
-    private fun OnSucces(it: WrapperTypeData) {
-        var newdata:WrapperTypeData
-        WrapperData.value.let {data->
-            newdata=WrapperTypeData(track = (data.track +it.track).toMutableList(), audiobook =(data.audiobook +it.audiobook).toMutableList() )
-
-        }
-        if (it.audiobook.isEmpty()) _SearchStates.value=SearchState.value.copy(audiobookEndReached = true)
-        if (it.track.isEmpty()) _SearchStates.value=SearchState.value.copy(trackEndReached = true)
-
-        _WrapperData.value=newdata
-        _SearchStates.value=SearchState.value.copy(isLoading = false)
-        currentPage+=50
-    }
 
     private fun onsearch(query: String) {
-        searchquery= query
-        searchJob?.cancel()
-        searchJob = viewModelScope.launch {
-            pageJob = null
-            currentPage=0
-            _WrapperData.value=WrapperTypeData()
-            LoadNewPage()
+        _SearchStates.value=SearchState.value.copy(isLoading = true)
+        viewModelScope.launch(Dispatchers.IO) {
+            val list = mutableListOf<MediaTypeDataClass>()
+            Constants.MediaType.forEach {type->
+                async {
+                    repository.Search(query = query, offset = 0, type = type).onEach { Results ->
+
+                        _SearchStates.value = SearchState.value.copy(isLoading = false)
+                        Results.onSuccess {
+                            list.add(MediaTypeDataClass(type, it.results))
+                        }
+                    }.launchIn(this)
+                }.await()
+
+            }
+
+            _MediaData.value = list
 
         }
     }
 }
+
+/*private fun LoadNewPage() {
+       if (pageJob?.isActive ?: false||searchquery.isBlank()) {
+           return
+       }
+       _SearchStates.value=SearchState.value.copy(isLoading = true)
+       pageJob = viewModelScope.launch {
+           repository.Search(searchquery, currentPage).onEach {
+
+               it.onSuccess {
+                   OnSucces(it)
+               }
+               it.onFailure {
+                   Log.d("hata",it.localizedMessage)
+                   _SearchStates.value=SearchState.value.copy(isLoading = false)
+               }
+
+           }.launchIn(this)
+
+
+       }
+   }
+
+   private fun OnSucces(it: WrapperTypeData) {
+       var newdata:WrapperTypeData
+       WrapperData.value.let {data->
+           newdata=WrapperTypeData(track = (data.track +it.track).toMutableList(), audiobook =(data.audiobook +it.audiobook).toMutableList() )
+
+       }
+       if (it.audiobook.isEmpty()) _SearchStates.value=SearchState.value.copy(audiobookEndReached = true)
+       if (it.track.isEmpty()) _SearchStates.value=SearchState.value.copy(trackEndReached = true)
+
+       _WrapperData.value=newdata
+       _SearchStates.value=SearchState.value.copy(isLoading = false)
+       currentPage+=50
+   }*/
