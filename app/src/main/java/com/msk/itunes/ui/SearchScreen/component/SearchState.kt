3deeb8f0a15b00d23f4c0a077817231db4f5c097
serhatkutlu <84921644+serhatkutlu@users.noreply.github.com>
2022-04-22 22:32:05 +0300

    unused varable deleted

--- app/src/main/java/com/msk/itunes/ui/SearchScreen/component/SearchState.kt
+++ app/src/main/java/com/msk/itunes/ui/SearchScreen/component/SearchState.kt
@@ -2,6 +2,5 @@ package com.msk.itunes.ui.SearchScreen.component
 
 data class SearchState(
     var isLoading:Boolean=false,
-    var trackEndReached:Boolean=false,
-    var audiobookEndReached:Boolean=false
+    var endReached:Boolean=false
 )
