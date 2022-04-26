208a809742011fbf1a2b9780c31f1e46591af6e1
serhatkutlu <84921644+serhatkutlu@users.noreply.github.com>
2022-04-26 15:22:11 +0300

    room ktx  dependency and primary key annotation added

--- app/src/main/java/com/msk/itunes/Data/Entity/SavedId.kt
+++ app/src/main/java/com/msk/itunes/Data/Entity/SavedId.kt
@@ -1,9 +1,11 @@
 package com.msk.itunes.Data.Entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 
 @Entity(tableName = "savedID")
 data class SavedId(
+    @PrimaryKey
     val id:Int
 )
